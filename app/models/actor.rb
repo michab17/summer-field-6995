c8867c5e17bfcf23c815dbd85a020f856eb1bf12
Micha Bernhard <mib70@pitt.edu>
2021-11-02 10:37:47 -0600

    complete extension using ruby

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,4 +1,15 @@
 class Actor < ApplicationRecord
   has_many :movie_actors
   has_many :movies, through: :movie_actors
+
+  def coactors #this is ruby and that is bad
+    coactors = []
+    movies.each do |movie|
+      movie.actors.each do |actor|
+        coactors << actor
+      end
+    end
+    coactors.delete(self)
+    coactors.uniq
+  end
 end
