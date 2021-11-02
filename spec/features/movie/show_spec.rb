f5b4e889075e2a2dd4e6f2673b33d7d4d089f2ff
Micha Bernhard <mib70@pitt.edu>
2021-11-02 10:06:47 -0600

    test user story 3

--- spec/features/movie/show_spec.rb
+++ spec/features/movie/show_spec.rb
@@ -42,4 +42,41 @@ RSpec.describe 'Movies Show Page' do
 
     expect(page).to have_content('The average age of actors is: 28.0')
   end
+
+  it 'does not display any actors not associated with the movie' do
+    studio1 = Studio.create!(name: 'MGM', location: 'California')
+    movie1 = studio1.movies.create!(title: 'movie1', creation_year: 1994, genre: "Horror")
+    kevin = Actor.create!(name: 'Kevin', age: 24)
+    micha = Actor.create!(name: 'Micha', age: 27)
+    amwake = Actor.create!(name: 'Amwake', age: 33)
+    jen = Actor.create!(name: 'Jen', age: 24)
+    MovieActor.create!(movie_id: movie1.id, actor_id: kevin.id)
+    MovieActor.create!(movie_id: movie1.id, actor_id: micha.id)
+    MovieActor.create!(movie_id: movie1.id, actor_id: amwake.id)
+
+    visit "/movies/#{movie1.id}"
+
+    expect(page).to_not have_content('Jen')
+  end
+
+  it 'has a form to add an actor to the movie' do
+    studio1 = Studio.create!(name: 'MGM', location: 'California')
+    movie1 = studio1.movies.create!(title: 'movie1', creation_year: 1994, genre: "Horror")
+    kevin = Actor.create!(name: 'Kevin', age: 24)
+    micha = Actor.create!(name: 'Micha', age: 27)
+    amwake = Actor.create!(name: 'Amwake', age: 33)
+    jen = Actor.create!(name: 'Jen', age: 24)
+    MovieActor.create!(movie_id: movie1.id, actor_id: kevin.id)
+    MovieActor.create!(movie_id: movie1.id, actor_id: micha.id)
+    MovieActor.create!(movie_id: movie1.id, actor_id: amwake.id)
+
+    visit "/movies/#{movie1.id}"
+
+    fill_in :name, with: 'Jen'
+    fill_in :age, with: '24'
+    click_button 'Submit'
+
+    expect(current_path).to eq("/movies/#{movie1.id}")
+    expect(page).to have_content("Jen")
+  end
 end
--- spec/models/movie_spec.rb
+++ spec/models/movie_spec.rb
@@ -6,4 +6,19 @@ RSpec.describe Movie do
     it { should have_many :movie_actors }
     it { should have_many(:actors).through(:movie_actors) }
   end
+
+  describe '#ordered_actors' do
+    it 'orders the actors from youngest to oldest' do
+      studio1 = Studio.create!(name: 'MGM', location: 'California')
+      movie1 = studio1.movies.create!(title: 'movie1', creation_year: 1994, genre: "Horror")
+      kevin = Actor.create!(name: 'Kevin', age: 24)
+      micha = Actor.create!(name: 'Micha', age: 27)
+      amwake = Actor.create!(name: 'Amwake', age: 33)
+      MovieActor.create!(movie_id: movie1.id, actor_id: kevin.id)
+      MovieActor.create!(movie_id: movie1.id, actor_id: micha.id)
+      MovieActor.create!(movie_id: movie1.id, actor_id: amwake.id)
+
+      expect(movie1.ordered_actors).to eq([kevin, micha, amwake])
+    end
+  end
 end
