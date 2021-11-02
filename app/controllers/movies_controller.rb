d7f8efb298ec1bb23eaa00a82e5e14feb5c5b884
Micha Bernhard <mib70@pitt.edu>
2021-11-02 10:06:57 -0600

    complete user story 3

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -2,5 +2,9 @@ class MoviesController < ApplicationController
   def show
     @movie = Movie.find(params[:id])
     @actors = @movie.ordered_actors
+    if params[:name]
+      new_actor = Actor.find_by name: params[:name], age: params[:age]
+      MovieActor.create!(movie_id: @movie.id, actor_id: new_actor.id)
+    end
   end
 end
